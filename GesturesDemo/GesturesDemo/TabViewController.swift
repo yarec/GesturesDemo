daa29f361a63d0c0d741817a1dffee7d94cf57fd
arslanbilal <bilal@bsglabs.pro>
2015-02-02 23:56:47 +0200

    Single Tap and  Double Tap Added

--- GesturesDemo/GesturesDemo/TabViewController.swift
+++ GesturesDemo/GesturesDemo/TabViewController.swift
@@ -8,6 +8,49 @@
 
 import UIKit
 
-class TabViewController: UIViewController {
+class TabViewController: UIViewController
+{
 
+    @IBOutlet var testView:UIView!
+    
+    override func viewDidLoad()
+    {
+        var tapGesture = UITapGestureRecognizer(target: self, action: "handleSingleTapGesture:")
+        testView.addGestureRecognizer(tapGesture)
+        
+        var doubleTapGesture = UITapGestureRecognizer(target: self, action: "handleDoubleTapGesture:")
+        doubleTapGesture.numberOfTapsRequired = 2
+        doubleTapGesture.numberOfTouchesRequired = 2
+        testView.addGestureRecognizer(doubleTapGesture)
+ 
+    }
+    
+    
+    func handleSingleTapGesture(tapGestureRecognizer:UITapGestureRecognizer) -> Void
+    {
+        var newWidth: CGFloat = 200.0
+        if testView.frame.size.width == newWidth
+        {
+            newWidth = 300.0
+        }
+        let currentCenter = testView.center
+        
+        testView.frame = CGRectMake(testView.frame.origin.x, testView.frame.origin.y, newWidth, testView.frame.height)
+        testView.center = currentCenter
+    }
+    
+    func handleDoubleTapGesture(tapGestureRecognizer:UITapGestureRecognizer) -> Void
+    {
+        var newSize = CGSizeMake(200.0, 200.0)
+        if  testView.frame.size == newSize
+        {
+            newSize.height = 300.0
+            newSize.width = 300.0
+        }
+        
+        let currentCenter = testView.center
+        
+        testView.frame = CGRectMake(testView.frame.origin.x, testView.frame.origin.y, newSize.width, newSize.height)
+        testView.center = currentCenter
+    }
 }
